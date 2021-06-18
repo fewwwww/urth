99b64af10286b529ef6621d9e172cebc76f1687d
Suning Yao <1651500561@qq.com>
2021-06-18 20:55:27 +0800

    built

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,6 +1,7 @@
 import Enterprise from '../imgs/enterprise.png'
 import Robo from '../imgs/robo.png'
 import Doge from '../imgs/doge.png'
+import Hack from '../imgs/hack.png'
 import '../components/Products.css'
 
 function Team() {
@@ -17,13 +18,13 @@ function Team() {
                 <img className='w-52 sm:w-auto sm:h-auto lg:px-40 py-20' src={Enterprise} alt="" />
             </div>
             <div style={{height: '400px'}} className='bg-gray-50 w-screen max-w-screen-xl mx-auto my-10 flex'>
-                <img className='w-52 sm:w-auto sm:h-auto lg:px-28 py-32 lg:py-8' src={Doge} alt="" />
+                <img className='w-52 sm:w-auto sm:h-auto lg:px-28 py-32 lg:py-8' src={Hack} alt="" />
                 <div className='w-1/2 h-full flex flex-col items-center justify-center text-left'>
-                    <div className='font-mono font-bold text-3xl p-5'>Bryce Zhu</div>
-                    <div className='font-mono text-xl p-2'>COO of URTH.ink</div>
-                    <div className='font-mono text-xl p-1'>C++ Developer</div>
-                    <div className='font-mono text-xl p-1'>IoT Device Expert</div>
-                    <div className='font-mono text-xl p-1'>MatLab Expert</div>
+                    <div className='font-mono font-bold text-3xl p-5'>Yufei Ma</div>
+                    <div className='font-mono text-xl p-2'>CMO of URTH.ink</div>
+                    <div className='font-mono text-xl p-1'>Economics Major</div>
+                    <div className='font-mono text-xl p-1'>Marketing Lead</div>
+                    <div className='font-mono text-xl p-1'>Finance Guru</div>
                 </div>
             </div>
             <div style={{height: '400px'}} className='bg-gray-50 w-screen max-w-screen-xl mx-auto my-10 flex'>
@@ -35,6 +36,16 @@ function Team() {
                     <div className='font-mono text-xl p-1'>Business Leader</div>
                 </div>
                 <img className='w-52 sm:w-auto sm:h-auto lg:px-48 py-2' src={Robo} alt="" />
+            </div>
+            <div style={{height: '400px'}} className='bg-gray-50 w-screen max-w-screen-xl mx-auto my-10 flex'>
+                <img className='w-52 sm:w-auto sm:h-auto lg:px-28 py-32 lg:py-8' src={Doge} alt="" />
+                <div className='w-1/2 h-full flex flex-col items-center justify-center text-left'>
+                    <div className='font-mono font-bold text-3xl p-5'>Bryce Zhu</div>
+                    <div className='font-mono text-xl p-2'>COO of URTH.ink</div>
+                    <div className='font-mono text-xl p-1'>C++ Developer</div>
+                    <div className='font-mono text-xl p-1'>IoT Device Expert</div>
+                    <div className='font-mono text-xl p-1'>MatLab Expert</div>
+                </div>
             </div>
       </div>
     );
