9a228c375e1575a3469ce83700becad0e79a15d7
Suning Yao <1651500561@qq.com>
2021-06-18 20:43:13 +0800

    built

--- src/components/Team.js
+++ src/components/Team.js
@@ -11,18 +11,18 @@ function Team() {
                     <div className='font-mono font-bold text-3xl p-5'>Suning Yao</div>
                     <div className='font-mono text-xl p-2'>CEO/CTO of URTH.ink</div>
                     <div className='font-mono text-xl p-1'>Ex-Microsoft Engineer</div>
-                    <div className='font-mono text-xl p-1'>Ex-Unionpay Blockchain Developer</div>
-                    <div className='font-mono text-xl p-1'>NYU Bachelor of Science in Math</div>
+                    <div className='font-mono text-xl p-1'>Ex-Unionpay Developer</div>
+                    <div className='font-mono text-xl p-1'>NYU BS in Math</div>
                 </div>
                 <img className='w-52 sm:w-auto sm:h-auto lg:px-40 py-20' src={Enterprise} alt="" />
             </div>
             <div style={{height: '400px'}} className='bg-gray-50 w-screen max-w-screen-xl mx-auto my-10 flex'>
                 <img className='w-52 sm:w-auto sm:h-auto lg:px-28 py-32 lg:py-8' src={Doge} alt="" />
-                <div className='w-1/2 h-full flex flex-col items-center justify-center'>
+                <div className='w-1/2 h-full flex flex-col items-center justify-center text-left'>
                     <div className='font-mono font-bold text-3xl p-5'>Bryce Zhu</div>
                     <div className='font-mono text-xl p-2'>COO of URTH.ink</div>
-                    <div className='font-mono text-xl p-1'>C++ Expert</div>
-                    <div className='font-mono text-xl p-1'>IoT Expert</div>
+                    <div className='font-mono text-xl p-1'>C++ Developer</div>
+                    <div className='font-mono text-xl p-1'>IoT Device Expert</div>
                     <div className='font-mono text-xl p-1'>MatLab Expert</div>
                 </div>
             </div>
@@ -30,7 +30,7 @@ function Team() {
                 <div className='w-1/2 h-full flex flex-col items-center justify-center'>
                     <div className='font-mono font-bold text-3xl p-5'>Jax Yan</div>
                     <div className='font-mono text-xl p-2'>CFO of URTH.ink</div>
-                    <div className='font-mono text-xl p-1'>Philosopher</div>
+                    <div className='font-mono text-xl p-1'>Philosopher Mind</div>
                     <div className='font-mono text-xl p-1'>Crypto Investor</div>
                     <div className='font-mono text-xl p-1'>Business Leader</div>
                 </div>
