59a21567fce69c287520a9826bc47cde882ec0ac
Suning Yao <1651500561@qq.com>
2021-06-18 21:11:07 +0800

    built

--- src/components/Team.js
+++ src/components/Team.js
@@ -18,13 +18,13 @@ function Team() {
                 <img className='w-52 sm:w-auto sm:h-auto lg:px-40 py-20' src={Enterprise} alt="" />
             </div>
             <div style={{height: '400px'}} className='bg-gray-50 w-screen max-w-screen-xl mx-auto my-10 flex'>
-                <img className='w-52 sm:w-auto sm:h-auto lg:px-28 py-32 lg:py-8' src={Hack} alt="" />
+                <img className='w-52 sm:w-7/12 sm:h-auto lg:px-28 py-32 lg:py-8' src={Hack} alt="" />
                 <div className='w-1/2 h-full flex flex-col items-center justify-center text-left'>
                     <div className='font-mono font-bold text-3xl p-5'>Yufei Ma</div>
                     <div className='font-mono text-xl p-2'>CMO of URTH.ink</div>
                     <div className='font-mono text-xl p-1'>Economics Major</div>
                     <div className='font-mono text-xl p-1'>Marketing Lead</div>
-                    <div className='font-mono text-xl p-1'>Finance Guru</div>
+                    <div className='font-mono text-xl p-1'>Finance Master</div>
                 </div>
             </div>
             <div style={{height: '400px'}} className='bg-gray-50 w-screen max-w-screen-xl mx-auto my-10 flex'>
